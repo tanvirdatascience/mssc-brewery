4a30787f67ad212980e057b15503a94e7a089952
rivnatdemha <tambir.ahmedcs@gmail.com>
2022-10-26 17:08:10 -0400

    Initialize

---
+++ src/main/java/guru/springframework/msscbrewery/MsscBreweryApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.msscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryApplication {
+
+	//main function
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryApplication.class, args);
+	}
+
+}
